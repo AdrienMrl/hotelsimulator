1de99621cc8da7a8b211f5c63ec8804b5fd1a0d4
Adrien morel <adrien.morel@me.com>
2016-03-19 20:33:21 -0700

    add some background music to the game

---
+++ Assets/engine/Engine.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+function RandomizeArray(arr : Array)
+{
+    for (var i = arr.length - 1; i > 0; i--) {
+        var r = Random.Range(0,i);
+        var tmp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = tmp;
+    }
+    return arr;
+}
+
+function playBackgroundMusic() {
+
+  var audio = gameObject.AddComponent.<AudioSource>();
+
+  var music_dir = Application.dataPath + "/Resources/Music";
+  var dirInfo = new System.IO.DirectoryInfo(music_dir).GetFiles("*.mp3");
+
+  while (true) {
+
+    dirInfo = RandomizeArray(dirInfo);
+
+    for (fileName in dirInfo) {
+      var filename = fileName.Name.Substring(0, fileName.Name.Length - 4);
+      audio.clip = Resources.Load("Music/" + filename, AudioClip);
+      audio.Play();
+      yield WaitForSeconds(audio.clip.length);
+    }
+  }
+
+
+}
+
+function Start () {
+  playBackgroundMusic();
+}
